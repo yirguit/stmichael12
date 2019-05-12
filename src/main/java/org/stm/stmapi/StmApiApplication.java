b8fdc2c18803accb0b132feffb2d5d8c1e5b2c78
yirguit <yirguit@gmail.com>
2019-05-12 14:36:26 -0400

    Initial Commit

---
+++ src/main/java/org/stm/stmapi/StmApiApplication.java
@@ -0,0 +1,13 @@
+package org.stm.stmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/stm/stmapi/StmApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.stm.stmapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StmApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
